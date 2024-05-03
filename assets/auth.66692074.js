aaf065eee4d238e22dac227648b50f1c3b5f436f
lulu-cao <lulu.cao.her@gmail.com>
2024-05-03 12:51:40 -0500

    deploy

--- assets/auth.66692074.js
+++
@@ -1,1 +0,0 @@
-import"./index.279c4a44.js";const a=localStorage.getItem("user");function o(e,t){e.authenticated===!1&&!a?t("/login"):t()}export{o as default};
---
+++ assets/auth.e99751b9.js
@@ -0,0 +1,1 @@
+import"./index.17db5d10.js";const a=localStorage.getItem("user");function o(e,t){e.authenticated===!1&&!a?t("/login"):t()}export{o as default};
