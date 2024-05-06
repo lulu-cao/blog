5c8729002203c2a9cf063236e2cbd3dc15bbfa55
lulu-cao <lulu.cao.her@gmail.com>
2024-05-05 19:52:49 -0500

    added 404 to handle refresh errors

--- src/router/index.js
+++ src/router/index.js
@@ -63,7 +63,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL), // production base url is set in vite.config.js
   routes,
 });
 
